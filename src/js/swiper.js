c4e5d1c156f01176078534f905b750a32d472a3e
htmlpluscss <79198889134@ya.ru>
2021-01-05 11:51:27 +0300

    swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -14,13 +14,36 @@
 			toggleSwipe = null,
 			resetSwipe = null;
 
-		const swipeNav = document.createElement('div'),
-			items = swipe.querySelectorAll('.swiper-slide'),
-			count = items.length,
-			cases = swipe.classList.contains('swiper-container--cases');
+		const swipeControls = document.createElement('div'),
+			  swipeNav = document.createElement('div'),
+			  swipeBtns = document.createElement('div'),
+			  swipeNext = document.createElement('button'),
+			  swipePrev = document.createElement('button'),
+			  items = swipe.querySelectorAll('.swiper-slide'),
+			  count = items.length,
+			  firstScreen = swipe.classList.contains('swiper-container--first-screen');
 
 		swipeNav.className = 'swiper-pagination';
-		swipe.parentNode.appendChild(swipeNav);
+		swipeControls.className = 'swiper-controls';
+
+		swipeBtns.className = 'swiper-navigation';
+		swipePrev.className = 'swiper-button-prev button';
+		swipeNext.className = 'swiper-button-next button';
+
+		swipePrev.innerHTML = '<svg width="16" height="29" viewBox="0 0 16 29"><path d="M0 14.034c0-.503.192-1.006.575-1.39L12.645.577a1.966 1.966 0 112.78 2.78L4.744 14.034l10.68 10.68a1.966 1.966 0 01-2.78 2.779L.574 15.423A1.96 1.96 0 010 14.035z"/></svg>';
+		swipeNext.innerHTML = '<svg width="16" height="29" viewBox="0 0 16 29"><path d="M16 14.034a1.96 1.96 0 01-.575 1.39L3.355 27.493a1.965 1.965 0 11-2.78-2.78l10.68-10.679L.575 3.356a1.966 1.966 0 012.78-2.78l12.07 12.069c.383.384.575.887.575 1.39z"/></svg>';
+
+		swipeBtns.appendChild(swipePrev);
+		swipeBtns.appendChild(swipeNext);
+		swipeControls.appendChild(swipeBtns);
+		swipeControls.appendChild(swipeNav);
+		swipe.parentNode.appendChild(swipeControls);
+
+		// eager
+		Array.from(swipe.querySelectorAll('[loading="lazy"]'), img => img.setAttribute('loading','eager'));
+
+		// hide
+		Array.from(items, el => el.classList.remove('hide'));
 
 		resetSwipe = () => {
 
@@ -32,44 +55,39 @@
 			}
 
 			swipeNav.classList.add('hide');
+			swipeBtns.classList.add('hide');
+			swipeControls.classList.add('hide');
 
 		}
 
 		resetSwipe();
 
-		if (cases) {
-
-			const current = document.querySelector('.cases__process-current');
+		if (firstScreen) {
 
 			toggleSwipe = () => {
 
-				resetSwipe();
+				toggleSwipe = null;
 
 				swipeNav.classList.remove('hide');
-				current.parentNode.classList.add('hide');
-				swipe.parentNode.classList.remove('swiper-container-style');
+				swipeBtns.classList.remove('hide');
+				swipeControls.classList.remove('hide');
+				swipe.parentNode.classList.add('swiper-container-style');
 
-				if(window.innerWidth < 768) {
-
-					swipe.parentNode.classList.add('swiper-container-style');
-					current.parentNode.classList.remove('hide');
-
-					mySwipe = new Swiper(swipe, {
-						loop: true,
-						autoHeight: true,
-						pagination: {
-							el: swipeNav,
-							bulletClass: 'button',
-							bulletActiveClass: 'is-active'
-						},
-						on: {
-							slideChangeTransitionEnd: () => {
-								current.textContent = (swipe.swiper.realIndex % count + 1);
-							}
-						}
-					});
-
-				}
+				new Swiper(swipe, {
+					loop: true,
+					slidesPerView: 3,
+					slidesPerGroup: 3,
+					spaceBetween: 27,
+					pagination: {
+						el: swipeNav,
+						bulletClass: 'button',
+						bulletActiveClass: 'is-active'
+					},
+					navigation: {
+						nextEl: swipeNext,
+						prevEl: swipePrev
+					}
+				});
 
 			}
 
