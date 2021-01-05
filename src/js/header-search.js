aa5dcd33769b5686b5fc7e8e855aa5239407b7c2
htmlpluscss <79198889134@ya.ru>
2021-01-05 08:36:39 +0300

    fix

--- src/js/header-search.js
+++ src/js/header-search.js
@@ -2,11 +2,17 @@
 
 	if(btn) {
 
-		const form = document.querySelector('.search-modal'),
-			  result = form.querySelector('.search-modal__result');
+		const form = document.querySelector('.header-search'),
+			  input = form.querySelector('.header-search__input'),
+			  result = form.querySelector('.header-search__result');
 
-		btn.addEventListener('click', () =>
-			document.body.classList.add('overlay', 'header-search-show'));
+		btn.addEventListener('click', () => {
+
+			document.body.classList.add('overlay', 'header-search-show');
+
+			setTimeout( () => input.focus(), 1000);
+
+		});
 
 		form.addEventListener('reset', () =>
 			document.body.classList.remove('overlay', 'header-search-show'));
