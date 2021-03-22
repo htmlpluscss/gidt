d4bd2b79cefa4a33fc8f35d05cc25a8f1f81335f
htmlpluscss <79198889134@ya.ru>
2021-03-22 21:11:42 +0300

    lk mobile

--- src/js/lk.js
+++ src/js/lk.js
@@ -1,6 +1,7 @@
 
-( () => {
+( lk => {
 
+	document.querySelector('.lk__btn-to-menu').addEventListener('click', () =>
+		lk.classList.add('left-menu-open'));
 
-
-})();
+})(document.querySelector('.lk'));
